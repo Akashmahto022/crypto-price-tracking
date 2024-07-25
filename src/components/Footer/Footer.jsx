85e121686891fdf948e2772c1cd036d6c3975b86
Akashmahto022 <akashmahto1169@gmail.com>
2024-07-25 10:41:14 +0530

    create navbar and the home page with api calling

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+
+  return (
+    <div className='footer'>
+        <p>Copyright @ 2024 , Crypto Price Tracking - All Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
